6492bfd9927d56fe24cbb3d1a3d2f0166b51296e
IlonaLukuts <40250049+IlonaLukuts@users.noreply.github.com>
2020-05-13 17:55:07 +0300

    init: start client app

---
+++ Client/src/javascript/components/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+ 
+export default class Greetings extends React.Component{
+    render(){
+        return <h1>Международный конкурс "WorldSkills" на базе ГрГУ им. Я. Купалы!</h1>;
+    }
+}
---
+++ Client/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/javascript/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', "@babel/preset-react"]
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.css"
+        })
+    ],
+
+    devServer: {  // configuration for webpack-dev-server
+        contentBase: './',  //source of static assets
+        port: 7700,
+        historyApiFallback: true,
+    }
+};
