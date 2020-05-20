e676deac6d61a86a8cab1b640ce56c0543d1579d
Anastasiya <anastasia.kramushchenka@gmail.com>
2020-05-20 12:22:55 +0300

    fix: change Get function

--- DAL/Repositories/ResultRepository.cs
+++ DAL/Repositories/ResultRepository.cs
@@ -13,11 +13,6 @@
         {
         }
 
-        public ResultEntity GetResultByIdAnswer(int id)
-        {
-            return Context.Results.AsNoTracking().FirstOrDefault(x => x.Answer.Id == id);
-        }
-
         public IEnumerable<ResultEntity> GetResultsByMark(float begin, float end)
         {
             return Context.Results.AsNoTracking().Where(x => x.Mark >= begin && x.Mark <= end);
