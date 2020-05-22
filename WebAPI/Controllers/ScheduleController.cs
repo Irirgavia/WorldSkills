076592dcf5322f7340cade5b9db82e07cb74cce6
Ilona Lukuts <ilona.lukut@gmail.com>
2020-05-22 13:58:10 +0300

    feat: update schedule controller

--- WebAPI/Controllers/ScheduleController.cs
+++ WebAPI/Controllers/ScheduleController.cs
@@ -8,38 +8,45 @@
     using System.Web.Http;
 
     using Models;
+    using ObjectMapper;
+    using BLL.Services;
 
     public class ScheduleController : ApiController
     {
         public IHttpActionResult Get()
         {
             ICollection<ScheduleElement> scheduleElements = new List<ScheduleElement>();
-            
-            return Json(Test());
+            var guestService = new GuestService("CompetitionContext");
+            var competitions = guestService.GetActualCompetitions();
+            foreach(var competition in competitions)
+            {
+                scheduleElements.Add(ObjectMapperDTOModel.ToModel(competition));
+            }
+            return Json(scheduleElements);
         }
 
-        private IEnumerable<ScheduleElement> Test()
+        /*private IEnumerable<ScheduleElement> Test()
         {
             ICollection<ScheduleElement> scheduleElements = new List<ScheduleElement>();
             var scheduleElement1 = new ScheduleElement()
             {
                 Skill = "skill1",
-                Stage = "stage1",
-                DateOfBegin = new DateTime(2019, 03, 12),
-                DateOfEnd = new DateTime(2019, 09, 20),
+                Stages = "stage1",
+                DateOfBegin = "2019, 03, 12",
+                DateOfEnd = "2019, 09, 20",
                 Address = "address1"
             };
             var scheduleElement2 = new ScheduleElement()
             {
                 Skill = "skill2",
-                Stage = "stage2",
-                DateOfBegin = new DateTime(2019, 03, 14),
-                DateOfEnd = new DateTime(2019, 09, 22),
+                Stages = "stage2",
+                DateOfBegin = "2019, 03, 14",
+                DateOfEnd = "2019, 09, 22",
                 Address = "address2"
             };
             scheduleElements.Add(scheduleElement1);
             scheduleElements.Add(scheduleElement2);
             return scheduleElements;
-        }
+        }*/
     }
 }
