82fedabad12bc113fb4a8c12109a6cd06b7551bd
Ilona Lukuts <ilona.lukut@gmail.com>
2020-05-22 13:59:02 +0300

    refactor: update structure of schedule element

--- WebAPI/Models/ScheduleElement.cs
+++ WebAPI/Models/ScheduleElement.cs
@@ -1,18 +1,33 @@
 ﻿namespace WebAPI.Models
 {
     using System;
-    
+    using System.Collections.Generic;
+
     public class ScheduleElement
     {
         public string Skill { get; set; }
 
-        public string Stage { get; set; }
+        public string DateOfBegin { get; set; }
 
-        public DateTime DateOfBegin { get; set; }
+        public string DateOfEnd { get; set; }
 
-        public DateTime DateOfEnd { get; set; }
+        public ICollection<CompetitionStage> Stages { get; set; }
 
-        public string Address { get; set; }
+        public class CompetitionStage
+        {
+            public string Type { get; set; }
+            public ICollection<StageTask> Tasks { get; set; }
 
+            public class StageTask
+            {
+                public string TaskDateOfBegin { get; set; }
+
+                public string TaskDateOfEnd { get; set; }
+
+                public bool IsActual { get; set; }
+
+                public ICollection<string> Addresses { get; set; }
+            }
+        }
     }
 }
