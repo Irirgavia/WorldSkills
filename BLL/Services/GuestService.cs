87649954a1a4588fc7f8fa681aeb313be0307ef9
Anastasiya <anastasia.kramushchenka@gmail.com>
2020-05-25 20:21:08 +0300

    feat: new function

--- BLL/Services/GuestService.cs
+++ BLL/Services/GuestService.cs
@@ -19,9 +19,27 @@
             this.unitOfWork = new UnitOfWork(connection);
         }
 
+        public void CreateUser(
+            string login,
+            string password,
+            string surname,
+            string name,
+            string patronymic,
+            DateTime birthday,
+            string photo,
+            string mail,
+            string telephone,
+            string awards)
+        {
+            var hashPassword = PasswordHasher.Hash(password);
+            var user = new UserEntity(login, hashPassword, Role.User, surname, name, patronymic, birthday, photo, mail, telephone, awards);
+            this.unitOfWork.UserRepository.Create(user);
+            this.unitOfWork.SaveChanges();
+        }
+
         public UserDTO GetUser(string login, string password)
         {
-            var user = this.unitOfWork.UserRepository.Get(u => u.Login.Equals(login)).FirstOrDefault();
+            var user = this.unitOfWork.UserRepository.GetUserByLogin(login);
             if (user == null)
             {
                 return null;
@@ -46,7 +64,17 @@
             return ObjectMapper<CompetitionEntity, CompetitionDTO>.MapList(
                 this.unitOfWork.CompetitionRepository.Get(c => c.DateTimeEnd >= DateTime.Now));
         }
-
+        
+        public IEnumerable<CompetitionDTO> GetCompetitionsBySkillAndYear(string skill, int year)
+        {
+            var competitionsEF = this.unitOfWork.CompetitionRepository.Get(c => 
+                c.Skill.Name == skill &&
+                (c.DateTimeBegin.Year == year - 1 || c.DateTimeBegin.Year == year)  &&
+                (c.DateTimeBegin.Year == year + 1 || c.DateTimeBegin.Year == year));
+            
+            return ObjectMapper<CompetitionEntity, CompetitionDTO>.MapList(competitionsEF);
+        }
+        
         public IEnumerable<CompetitionDTO> GetCompetitionsBySkill(string skill)
         {
             return ObjectMapper<CompetitionEntity, CompetitionDTO>.MapList(
