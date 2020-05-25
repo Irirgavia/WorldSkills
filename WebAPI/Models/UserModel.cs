800999f365a7876396c161fc2994d570b91b8636
Ilona Lukuts <ilona.lukut@gmail.com>
2020-05-25 20:17:57 +0300

    feat: add new controller and model for user

---
+++ WebAPI/Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Role { get; set; }
+
+        public AutorizationStatus Status { get; set; }
+    }
+
+    public enum AutorizationStatus
+    {
+        Success,
+        NotFound,
+        WrongPassword
+    }
+}
