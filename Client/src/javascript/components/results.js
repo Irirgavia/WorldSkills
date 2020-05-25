8d7bacd953b895233a6b098f42dc184b768672d4
Ilona Lukuts <ilona.lukut@gmail.com>
2020-05-25 20:15:04 +0300

    fix: update structure of component "results"

--- Client/src/javascript/components/results.js
+++ Client/src/javascript/components/results.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ResultSearch from './resultpath/resultsearch.js';
 
 export default class Results extends React.Component {
     render() {
@@ -15,27 +13,3 @@ export default class Results extends React.Component {
         </form>
     }
 }
-
-
-/*<div>
-            <table>
-                <caption>Результаты</caption>
-                <tr>
-                    <td><label for = "skill">Профессия: </label></td>
-                    <td><label for = "stage">Этап: </label></td>
-                    <td><label for = "year">Год: </label></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td><input  type = "text" id = "skill" name = "skill" /></td>
-                    <td><input  type = "text" id = "stage" name = "stage" /></td>
-                    <td><input  type = "text" id = "year" name = "year" /></td>
-                    <td><button id = "search" onClick = {resultSearchOnClick()}>Искать</button></td>
-                    <td><button id = "download" onClick = "" disabled>Скачать</button></td>
-                </tr>
-            </table>
-            <div id="results">
-
-            </div>
-        </div>*/
