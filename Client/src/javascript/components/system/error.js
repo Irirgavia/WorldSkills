e63a807adf90560fcfbb47021aef427182ba347b
Ilona Lukuts <ilona.lukut@gmail.com>
2020-05-29 20:50:45 +0300

    feat: add system components

---
+++ Client/src/javascript/components/system/error.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Error extends React.Component{
+    render(){
+        var mes = '';
+        switch(this.props.error)
+        {
+            case "Password is wrong.":
+                {
+                    mes = "Неправильно введен пароль.";
+                }
+            case "User is not found.":
+                {
+                    mes = "Пользователь с таким логином не найден.";
+                }
+        }
+        return <div id="error">Ошибка: {mes}</div>;
+    }
+}
