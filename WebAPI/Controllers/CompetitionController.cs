88b367a39167415c4e5b7a5175c4ffddb1aa5307
Ilona Lukuts <ilona.lukut@gmail.com>
2020-06-01 20:32:10 +0300

    feat: add new controllers

---
+++ WebAPI/Controllers/CompetitionController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models.RequestModels;
+
+namespace WebAPI.Controllers
+{
+    using BLL.DTO.Competition;
+    using ServiceProvider;
+    using System.Data.Entity.Core.Objects;
+    using WebAPI.ObjectMapper;
+
+    public class CompetitionController : ApiController
+    {
+        [Route("api/competitions/admin")]
+        public IHttpActionResult GetByAdmin([FromBody] int adminId)
+        {
+            var service = ServiceProvider.GetGuestService();
+            var competitions = service.GetAllCompetitions();
+            ICollection<Models.ResponseModels.ForAdmin.CompetitionResponseModel> response = new List<Models.ResponseModels.ForAdmin.CompetitionResponseModel>();
+            foreach(var competition in competitions)
+            {
+                response.Add(ObjectMapperDTOModelForAdmin.ToCompetitionForAdminResponseModel(competition));
+            }
+            return Json(response);
+        }
+
+        [Route("api/competitions/participant")]
+        public IHttpActionResult GetByParticipant([FromBody] int participantId)
+        {
+            var adminService = ServiceProvider.GetAdministratorService();
+            var stages = adminService.GetStagesByAccountId(participantId);
+            ICollection<Models.ResponseModels.ForParticipant.CompetitionForTaskResponseModel> response = new List<Models.ResponseModels.ForParticipant.CompetitionForTaskResponseModel>();
+            foreach (var stage in stages)
+            {
+                response.Add(ObjectMapperDTOModelForParticipant.ToCompetitionForParticipantResponseModel(stage));
+            }
+            return Json(response);
+        }
+
+        public IHttpActionResult Save([FromBody] CompetitionSaveRequestModel parameters)
+        {
+            var adminService = ServiceProvider.GetAdministratorService();
+            if (parameters.competitionId == -1)
+                adminService.CreateCompetition(
+                    ObjectMapperDTOModel.ParseToDateTime(parameters.dateOfBegin),
+                    ObjectMapperDTOModel.ParseToDateTime(parameters.dateOfEnd),
+                    parameters.skill);
+            else
+            {
+                var competitionDTO=adminService.GetCompetitionById(parameters.competitionId);
+                competitionDTO.DateTimeBegin = ObjectMapperDTOModel.ParseToDateTime(parameters.dateOfBegin);
+                competitionDTO.DateTimeEnd = ObjectMapperDTOModel.ParseToDateTime(parameters.dateOfEnd);
+                if (competitionDTO.Skill.Name != parameters.skill)
+                {
+                    var skillDTO = adminService.GetSkillByName(parameters.skill);
+                    if (skillDTO == null)
+                    {
+                        adminService.CreateSkill(parameters.skill);
+                    }
+                    competitionDTO.Skill = skillDTO;
+                }
+                adminService.UpdateCompetition(competitionDTO);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/Controllers/StageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models.RequestModels;
+
+namespace WebAPI.Controllers
+{
+    public class StageController : ApiController
+    {
+        public IHttpActionResult Save([FromBody] StageSaveRequestModel parameters)
+        {
+            return BadRequest();
+        }
+    }
+}
