96c028ae95b8d123928b74390f0b60f1a5d270fc
Anastasiya <anastasia.kramushchenka@gmail.com>
2020-06-02 04:29:11 +0300

    feat: add basic data of competition

--- DAL/Contexts/Initializers/CompetitionInitializer.cs
+++ DAL/Contexts/Initializers/CompetitionInitializer.cs
@@ -1,10 +1,14 @@
 ﻿namespace DAL.Contexts.Initializers
 {
+    using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
+    using System.Linq;
 
+    using DAL.Entities.Account;
     using DAL.Entities.Competition;
 
-    public class CompetitionInitializer : DropCreateDatabaseAlways<CompetitionContext>
+    public class CompetitionInitializer : DropCreateDatabaseIfModelChanges<CompetitionContext>
     {
         protected override void Seed(CompetitionContext context)
         {
@@ -20,6 +24,37 @@
             context.StageTypes.Add(new StageTypeEntity("International"));
 
             context.SaveChanges();
+
+            context.Skills.Add(new SkillEntity("cooker"));
+            context.SaveChanges();
+
+            var skill = context.Skills.Where(s => s.Name == "cooker");
+            context.Competitions.Add(
+                new CompetitionEntity(
+                    1,
+                    new DateTime(2018, 2, 2),
+                    new DateTime(2020, 10, 3),
+                    new List<StageEntity>()));
+            context.SaveChanges();
+
+            context.Stages.Add(new StageEntity(1, 1, new List<TaskEntity>(), new List<int>() { 1 }));
+            context.Stages.Add(new StageEntity(1, 2, new List<TaskEntity>(), new List<int>() { 1 }));
+            context.Stages.Add(new StageEntity(1, 3, new List<TaskEntity>(), new List<int>() { 1 }));
+            context.Stages.Add(new StageEntity(1, 4, new List<TaskEntity>(), new List<int>() { 1 }));
+            context.SaveChanges();
+
+            var address = new AddressEntity("rb", "grodno", "ogeshko", "32", "1", "notes");
+            context.Tasks.Add(
+                new TaskEntity(
+                    1,
+                    new DateTime(2018, 2, 21),
+                    new TimeSpan(0, 4, 0),
+                    "desc",
+                    "link",
+                    new List<AddressEntity>() { address },
+                    new List<AnswerEntity>()));
+            context.SaveChanges();
+
         }
     }
 }
